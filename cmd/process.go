e25759b170a8f8a6f22861c1bdeb3b5342195319
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-10-11 23:53:42 +0300

    AbortProcess method

--- cmd/process.go
+++ cmd/process.go
@@ -1,4 +1,22 @@
 package cmd
 
+import "syscall"
+
 type Process struct {
+	Pid int
+}
+
+// AbortProcess terminates the process associated with the Process instance.
+// It sends a SIGTERM signal to the process group identified by the process ID (Pid).
+// This method is used to request graceful termination of the process.
+func (p *Process) AbortProcess() error {
+	// Retrieve the process ID (Pid) from the Process instance.
+	// This ID represents the specific process that will be terminated.
+	pid := p.Pid
+
+	// Send a SIGTERM signal to the process group that the process belongs to.
+	// The '-' before the pid indicates that the signal should be sent to the entire process group
+	// instead of just the single process. This is useful when the process has spawned child processes,
+	// allowing all related processes to be terminated together.
+	return syscall.Kill(-pid, syscall.SIGTERM)
 }
