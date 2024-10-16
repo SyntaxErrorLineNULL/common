24f8ccf2bf34435962a6a3813f3fcfa5bc6212e7
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-10-16 22:25:01 +0300

    base constructor and Invoke method

--- http/http.go
+++ http/http.go
@@ -1,1 +1,16 @@
 package http
+
+import (
+	"context"
+	"sync"
+)
+
+type Client struct {
+	wg *sync.WaitGroup
+}
+
+func NewClient() *Client {
+	return &Client{wg: &sync.WaitGroup{}}
+}
+
+func (c *Client) Invoke(ctx context.Context) {}
