e8211f0df3c6d4c28448f45b81124145652b43e7
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-11-11 23:36:55 +0300

    write

--- buffer/bytebuffer.go
+++ buffer/bytebuffer.go
@@ -1,9 +1,14 @@
 package buffer
 
 type ByteBuffer struct {
-	Bytes []byte
+	bytes []byte
 }
 
 func (b *ByteBuffer) Len() int {
-	return len(b.Bytes)
+	return len(b.bytes)
+}
+
+func (b *ByteBuffer) Write(data []byte) (int, error) {
+	b.bytes = append(b.bytes, data...)
+	return len(b.bytes), nil
 }
