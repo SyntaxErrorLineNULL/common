7c4f02f3e124856d1b847cc42d56531101123da9
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-11-11 23:45:47 +0300

    Bytes

--- buffer/bytebuffer.go
+++ buffer/bytebuffer.go
@@ -1,6 +1,8 @@
 package buffer
 
-import "errors"
+import (
+	"errors"
+)
 
 type ByteBuffer struct {
 	bytes []byte
@@ -19,7 +21,13 @@ func (b *ByteBuffer) Read(p []byte) (int, error) {
 	if len(b.bytes) == 0 {
 		return 0, errors.New("buffer is empty")
 	}
+
 	n := copy(p, b.bytes)
 	b.bytes = b.bytes[n:]
+
 	return n, nil
+}
+
+func (b *ByteBuffer) Bytes() []byte {
+	return b.bytes
 }
